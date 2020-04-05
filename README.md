3ed1863457baa1683127fbc11da5d27f286bc07f
Felice Serena <felice@serena-mueller.ch>
2020-04-05 16:30:58 +0200

    Install instructions in README added.

--- README.md
+++ README.md
@@ -1,5 +1,22 @@
 # Ring Ruler
 
-This add ons for Blender helps in the creation of rings with text written on them. 
+This add on for Blender helps in the creation of rings with text written on them. 
+
 They are used to mark birds when they are young.
+
+## Installation
+
+Blender add ons are nothing more than python scripts stored in `scripts/addons/` within blender's configuration.
+
+Download and zip the `ring_ruler` directory in this repository. In Blender, go to "Preferences -> Add-ons" and click "Install".
+
+A file browser pops up. Pick the zipped version of the `ring_ruler` directory and hit "Install Add-on". 
+
+Blender will automatically unpack the zip and store all files at the right place in `scripts/addons/ring_ruler`.
+
+## Usage
+
+In Blender, search for "Ring Ruler" or go to "Object -> Ring Ruler". A helper window with settings will pop up.
+
+The rings are created in original size (millimeters). The default cube measures 1x1x1 meter, so it covers the created rings. Delete/move it and zoom towards the center.
 
--- ring_ruler/ring_ruler.py
+++ ring_ruler/ring_ruler.py
@@ -8,9 +8,9 @@ from .ring_factory import RingFactory
 from .utils import log
 
 
-def arrange_in_plane(rings):
+def arrange_in_plane(rings, width, height):
     plane_pos = Vector((0,0))
-    plane = Vector((0.2, 0.2))
+    plane = Vector((width, height))
     
     margin = (0.003,0.003)
     
@@ -52,6 +52,8 @@ class RingRulerOperator(bpy.types.Operator):
     begin: bpy.props.IntProperty(name="Begin", default=1, min=0, max=99999)
     end: bpy.props.IntProperty(name="End", default=3, min=0, max=99999)
     year: bpy.props.IntProperty(name="Year", default=datetime.datetime.now().year%100, min=0, max=99)
+    workspace_width: bpy.props.IntProperty(name="Print width", default=200, min=0, max=999)
+    workspace_height: bpy.props.IntProperty(name="Print height", default=200, min=0, max=999)
     zero_fill: bpy.props.IntProperty(name="Fill zeros", default=4, min=0, max=6) 
 
     def log(self, msg):
@@ -86,7 +88,7 @@ class RingRulerOperator(bpy.types.Operator):
         rings = self.define_instanced_rings()
 
         self.log("Arranging ring layout ...")
-        arrange_in_plane(rings)
+        arrange_in_plane(rings, 0.001*self.workspace_width, 0.001*self.workspace_height)
 
         rf = RingFactory(False)
         rf.create_rings(context, rings)
